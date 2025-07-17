8c0868621eb935f8eae8977778853525a6a5f370
Deepanshu Singh <deepdude61@gmail.com>
2025-07-17 17:11:18 +0530

    Conditional Statement useing ReadlineSync

---
+++ Conditional.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+
+const number = Number(readlineSync.question("Enter a number: "));
+const remainderAfterDivisionByThree = number % 3;
+const remainderAfterDivisionByFive = number % 5;
+
+if (remainderAfterDivisionByThree === 0 && remainderAfterDivisionByFive === 0) {
+    console.log("Fizz");
+} else {
+    console.log("The number is not divisible by both 3 and 5.");
+}
